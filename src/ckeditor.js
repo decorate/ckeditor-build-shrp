68e5decdd8c32b5b7a27c6d0567db5f3dee3b695
ishizuka takeaki <>
2020-11-17 13:21:55 +0900

    heading 修正

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -140,13 +140,13 @@ ClassicEditor.defaultConfig = {
         		},
         		{
         			model: 'heading4',
-        			view: 'h3',
+        			view: 'h4',
         			title: 'Heading 4',
         			class: 'ck-heading_heading4'
         		},
         		{
         			model: 'heading5',
-        			view: 'h4',
+        			view: 'h5',
         			title: 'Heading 5',
         			class: 'ck-heading_heading5'
         		},
